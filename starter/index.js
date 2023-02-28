11b61f5af97e61899e0121bc60a3b3fa16a548db
onanike obinna <onanikefrancis@gmail.com>
2023-02-28 05:37:10 +0000

    added_files_from_the_starter_files

---
+++ starter/index.js
@@ -0,0 +1,34 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+const teamMemebers = [];
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const promptManager = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "what",
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+};
